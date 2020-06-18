c7470d42ef48624849a7a48551593ca283f3a21c
Evgeniy <eafonasyev@gmail.com>
2020-06-18 08:35:40 +0600

    first commit entity

---
+++ src/main/java/com/eafonasyev/firstGuru5/firstGuru5/FirstGuru5Application.java
@@ -0,0 +1,13 @@
+package com.eafonasyev.firstGuru5.firstGuru5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGuru5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstGuru5Application.class, args);
+	}
+
+}
